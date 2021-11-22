258d3d75d88638d3c8317118b787c2de963c59c4
alex123aaaaaaaaaaaaaaaaaaaaaaaa <92975320+alex123aaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-11-22 16:46:10 +0100

    Add files via upload

---
+++ javascripts/halloween.js
@@ -0,0 +1,14 @@
+function mostrar(imagen){
+    document.getElementById(imagen).style.visibility="visible";
+}
+
+function ocultar(imagen){
+    document.getElementById(imagen).style.visibility="hidden";
+}
+
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
+function pausar(sonido){
+    document.getElementById(sonido).pause();
+}
